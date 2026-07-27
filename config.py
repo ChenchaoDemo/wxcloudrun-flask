6d3654e7d7c169b7b0cff18fa64eb979afff26d2
ADMIN\56871 <568718926@qq.com>
2026-07-27 16:35:53 +0800

    no message

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ DEBUG = True
 
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+password = os.environ.get("MYSQL_PASSWORD", 'Aa568718')
+db_address = os.environ.get(
+    "MYSQL_ADDRESS",
+    'sh-cynosdbmysql-grp-qj3bw2vq.sql.tencentcdb.com:26882',
+)
